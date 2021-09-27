5898ebbadc4ac5225e1e17f759b69b56fdc9eff7
ClaudiaLanzoni <claudia.lanzoni1991@gmail.com>
2021-09-27 17:42:49 +0200

    dynamic content initialization

---
+++ src/components/Card.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card_element">
+            <div></div>
+        </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+@import "../style/variables.scss";
+@import "../style/general.scss";
+
+    .card_element {
+        height: 100px;
+        width: 100px;
+        background-color: rgb(46, 224, 224);
+        border: 5px solid black;
+        flex-basis: calc(100% / 6);
+        margin: 0 auto
+    }
+</style>
