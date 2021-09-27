8bc23c0a28a988fb3a549c809e64101b0c0c54c4
ClaudiaLanzoni <claudia.lanzoni1991@gmail.com>
2021-09-27 15:37:10 +0200

    finalized static jumbotron

---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="jumbotron_size">
+     
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+@import "../style/variables.scss";
+@import "../style/general.scss";
+
+    .jumbotron_size {
+        height: 300px;
+        background-image: url("../assets/vue-dc-comics-1/img/jumbotron.jpg");
+        background-size: cover;
+        background-repeat: no-repeat;
+    }
+
+
+</style>
